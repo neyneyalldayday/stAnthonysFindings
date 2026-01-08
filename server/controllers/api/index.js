07acd29f3f16a43fa2b5a5211999d8ee628e6ae1
neyneyalldayday <drfrankenstein123@gmail.com>
2026-01-07 19:40:25 -0600

    getting top donations and the total ammount from the api

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router()
 const donationController = require('./donation-controller');
-
-
+const recordController = require('./records-controller');
 
 router.use('/donation', donationController);
-
+router.use('/records', recordController);
 
  
 
---
+++ server/controllers/api/records-controller.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const { Sequelize } = require('sequelize');
+const Donation = require('../../models/Donation');
+
+
+
+router.get("/donationInfo", async (req, res) => {
+   try {
+    const allDonations = await Donation.findAll();
+    res.status(200).json(allDonations);
+   } catch (err) {
+    console.error(err);
+    
+   }
+})
+
+
+router.get('/donationTotal', async (req,res)  => {
+    try {
+        const result = await Donation.findOne({
+            attributes: [
+                [Sequelize.fn('SUM', Sequelize.cast(Sequelize.col('amount'),
+                     'DECIMAL(10,2)')), 
+                     'totalAmount'
+                ]               
+            ],
+            raw: true
+        });
+
+        const total = parseFloat(result.totalAmount) || 0;
+
+
+        res.status(200).json({
+            totalAmount : total,
+            formatedTotal:  `$${total.toFixed(2)}`
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'failed to calculate total donations' });
+    }
+});
+
+
+router.get('/topDonations', async (req, res) => {
+    try {
+        const topDonations = await Donation.findAll({
+            attributes: [
+                'id',
+                'amount',
+                'donor_name',
+                'email',
+                'stripe_payment_intent_id',
+                
+            ],
+            order: [
+                [Sequelize.cast(Sequelize.col('amount'), 'DECIMAL(10,2)'), 'DESC']
+            ],
+            limit:5,
+            raw:true
+        });
+
+
+        const formattedDonations = topDonations.map(donation => ({
+            ...donation,
+            amount: parseFloat(donation.amount) || 0,
+            formattedAmount: `$${(parseFloat(donation.amount) || 0).toFixed(2)}`
+        }));
+
+
+        res.status(200).json({
+            topDonations: formattedDonations,
+            count: formattedDonations.length
+        })
+
+
+
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch top donations'});
+        
+    }
+})
+
+
+
+
+
+
+module.exports = router;
