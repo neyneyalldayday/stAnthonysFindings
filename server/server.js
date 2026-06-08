294b3370caa7a9ba8f6aec7d697d2551da415d50
neyneyalldayday <drfrankenstein123@gmail.com>
2026-06-08 15:33:14 -0500

    un commented the line for production

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use(routes);
 
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 sequelize.sync({ alter: true })
   .then(() => {
